a45258adea28de68f446be770532f633933b76b9
Stanislav <77130336+standbit@users.noreply.github.com>
2021-11-30 21:07:35 +0300

    Add 6 hats

--- main.py
+++ main.py
@@ -10,14 +10,40 @@ env = Environment(
 
 template = env.get_template('template.html')
 
-rendered_page = template.render(
-    cap1_title="Красная кепка",
-    cap1_text="$ 100.00",
-    cap2_title="Чёрная кепка",
-    cap2_text="$ 120.00",
-    cap3_title="Ещё одна чёрная кепка",
-    cap3_text="$ 90.00",
-)
+caps = [
+{
+        "title": "Красная кепка",
+        "price": "$ 100.00",
+        "image": "https://dvmn.org/filer/canonical/1569333143/329/"
+    },
+    {
+        "title": "Чёрная кепка",
+        "price": "$ 120.00",
+        "image": "https://dvmn.org/filer/canonical/1569333143/330/"
+    },
+    {
+        "title": "Ещё одна чёрная кепка",
+        "price": "$ 90.00",
+        "image": "https://dvmn.org/filer/canonical/1569333144/331/"
+    },
+    {
+        "title": "Камуфляжная кепка",
+        "price": "$ 100.00",
+        "image": "https://dvmn.org/filer/canonical/1569943835/350/"
+    },
+    {
+        "title": "Джинсовая кепка",
+        "price": "$ 100.00",
+        "image": "https://dvmn.org/filer/canonical/1569943835/351/"
+    },
+    {
+        "title": "Плоская кепка",
+        "price": "$ 100.00",
+        "image": "https://dvmn.org/filer/canonical/1569943836/352/"
+    }
+]
+
+rendered_page = template.render(caps=caps)
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
